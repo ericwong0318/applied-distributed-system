6f7c4a2e190d3a09ec23349addeefdc039d0659b
Eric Wong <ericwong0318@gmail.com>
2022-11-26 10:15:06 +0800

    fix: logout

--- frontend/src/FixedAppBar.tsx
+++ frontend/src/FixedAppBar.tsx
@@ -2,11 +2,22 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Cookies from 'js-cookie';
+import {useNavigate} from "react-router-dom";
 
 export function FixedAppBar(props: {
     zIndex: (theme: any) => number, auth: boolean,
     onClick: (event: React.MouseEvent<HTMLElement>) => void, anchorEl: HTMLElement | null, onClose: () => void
 }) {
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        Cookies.remove("tokenString");
+        navigate("/");
+    }
+
     return <>
         {/*appbar*/}
         <AppBar position="fixed" sx={{zIndex: props.zIndex}}>
@@ -14,6 +25,10 @@ export function FixedAppBar(props: {
                 <Typography variant="h6" noWrap component="div">
                     Chat
                 </Typography>
+                {" "}
+                <Box sx={{justifyContent: 'flex-end'}}>
+                    <Button color="inherit" onClick={handleLogout}>Logout</Button>
+                </Box>
             </Toolbar>
         </AppBar>
     </>;
