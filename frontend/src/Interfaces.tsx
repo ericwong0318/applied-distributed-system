a93d6a841e95700393ef56630aa1099f3672200d
Eric Wong <ericwong0318@gmail.com>
2022-11-30 21:39:42 +0800

    feat: user and channel interface

--- frontend/src/Interfaces.tsx
+++ frontend/src/Interfaces.tsx
@@ -1,7 +1,19 @@
+export interface UserInterface {
+    email: string,
+    password: string,
+    channels: number[]
+}
+
 export interface MessageInterface {
     messageId: string,
     email: string,
     channelId: number,
     time: string,
     content: string
+}
+
+export interface ChannelInterface {
+    channelId: number,
+    channelName: string,
+    wiki: string
 }
