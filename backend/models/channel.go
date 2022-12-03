e98a1952dc1de997cbd0e2ca44792462969451f1
Eric Wong <ericwong0318@gmail.com>
2022-12-03 13:13:00 +0800

    feat: add field Wiki to struct Channel

--- backend/models/channel.go
+++ backend/models/channel.go
@@ -3,4 +3,5 @@ package models
 type Channel struct {
 	ChannelId   int    `form:"channelId" bson:"channelid" json:"channelId"`
 	ChannelName string `form:"channelName" bson:"channelname" json:"channelName"`
+	Wiki        string `form:"wiki" bson:"wiki" json:"wiki"`
 }
